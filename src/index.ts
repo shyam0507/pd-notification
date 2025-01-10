25bcfe0ccfc6cebe0e4c83368ffd191d7c51c2ab
shyam0507 <shyam432168@gmail.com>
2025-01-10 15:36:54 +0530

    docker file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { OrderConfirmedConsumer } from "./events/order-confirmed-consumer";
 import mongoose from "mongoose";
-const { Kafka, logLevel } = require('kafkajs')
+const { Kafka, logLevel } = require("kafkajs");
 
 const app = express();
 
@@ -9,22 +9,25 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World, from pd-notification!");
 });
 
-
 //Kafka consumer and producer
 const kafka = new Kafka({
-    logLevel: logLevel.INFO,
-    brokers: [`localhost:9092`],
-    clientId: 'notification-consumer',
-  })
+  logLevel: logLevel.INFO,
+  brokers: [process.env.KAFKA_BROKERS as string],
+  clientId: "notification-consumer",
+  sasl: {
+    mechanism: "plain",
+    username: process.env.KAFKA_USERNAME,
+    password: process.env.KAFKA_PASSWORD,
+  },
+});
 
-  
-const consumer = kafka.consumer({ groupId: 'notification-group' })
-consumer.connect()
+const consumer = kafka.consumer({ groupId: "notification-group" });
+consumer.connect();
 
- new OrderConfirmedConsumer(consumer).start();
+new OrderConfirmedConsumer(consumer).start();
 
- mongoose.connect('mongodb://127.0.0.1:27017/pd_notifications');
- 
+mongoose.connect(process.env.MONGO_URI + "/pd_notifications");
+
 app.listen(3007, () => {
   console.log("Server is running on port 3007");
 });
